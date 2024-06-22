a021615a9b7a1c135cddcfec33b2078f1a03d8af
TorsteinM <torsteinem@gmail.com>
2024-06-22 15:20:55 +0200

    Problem 13 / Fillesning

--- src/problems/problem13.rs
+++ src/problems/problem13.rs
@@ -2,15 +2,35 @@
 /// Problem 13: Sum av store tall
 /// 
 
-use std::{fs::File, io::BufRead};
+use std::fs::File;
+use std::io::{self, BufRead};
 
 pub fn solve() -> u64 {
-    let file = File::open("src/problems/problem13.txt").unwrap();
-    let reader = std::io::BufReader::new(file);
-    let mut sum: u128 = 0;
-    for line in reader.lines() {
-        let number = line.unwrap().parse::<u128>().unwrap();
-        sum += number;
+
+    let path = "src/problems/problem13.txt";
+
+    let input = File::open(path).unwrap();
+    let reader = io::BufReader::new(input);
+    let lines: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
+    // log2(10^50) = 50*log2(10) = 166.09640474436813   u128 holder ikke
+    // log2(10^25) = 25*log2(10) = 83.04820237218406    u128 holder
+    let mut lower_sum: u128 = 0;
+    let mut upper_sum: u128 = 0;
+    for line in lines {
+        lower_sum += line[25..].parse::<u128>().unwrap();
+        upper_sum += line[..25].parse::<u128>().unwrap();
     }
-    sum.to_string()[0..10].parse::<u64>().unwrap()
+    upper_sum += lower_sum / 10u128.pow(25);
+    
+    (upper_sum / 10u128.pow(17)) as u64 // NB! 10^17 er hardkodet for akkurat disse tallene.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(solve(), 5537376230);
+    }
 }
